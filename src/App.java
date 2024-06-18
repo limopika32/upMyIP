25f0138e3264fd249eb076889cb8282b88ba9bf5
limop <c1104121@planet.kanazawa-it.ac.jp>
2024-06-18 14:19:26 +0900

    add Notification

--- src/App.java
+++ src/App.java
@@ -4,33 +4,74 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
 
+import java.awt.TrayIcon.MessageType;
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        String ip = getIP(NetworkInterface.getNetworkInterfaces());
-        if (ip == null) System.exit(1);
-
-        String full_addr = "full address:s:" + ip;
-
-        String default_properties = """
-        screen mode id:i:1
-        use multimon:i:0
-        desktopwidth:i:1280
-        desktopheight:i:720""";
-        
+    SystemTray tray = SystemTray.getSystemTray();
+    Image image = Toolkit.getDefaultToolkit().createImage( ClassLoader.getSystemResource("icon.png") );
+    TrayIcon icon = new TrayIcon(image);
+    
+    public App() throws Exception{
+        // Nortification init
+        icon.setImageAutoSize(true);
         try {
-            PrintWriter pw = new PrintWriter("./Connect.rdp");
-
-            pw.println(full_addr);
-            pw.println(default_properties);
-
-            pw.close();
-        } catch (IOException e) {
+            tray.add(icon); // システムトレイに追加
+        } catch (AWTException e) {
             e.printStackTrace();
         }
 
+        // main 
+        String ip = getIP(NetworkInterface.getNetworkInterfaces());
+        if (ip == null){
+            Nortification(1);
+            System.exit(1);
+        }
+        
+        String full_addr = "full address:s:" + ip;
+        
+        String default_properties = """
+            screen mode id:i:1
+            use multimon:i:0
+            desktopwidth:i:1280
+            desktopheight:i:720""";
+            
+        try {
+            PrintWriter pw = new PrintWriter("./Connect.rdp");
+                
+            pw.println(full_addr);
+            pw.println(default_properties);
+                
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Nortification(2);
+        }
+
+        Nortification(ip);
+        
+
+        // exit
+        Thread.sleep(500);
+        tray.remove(icon);
     }
 
-    static String getIP(Enumeration<NetworkInterface> ifs) {
+    public static void main(String[] args) throws Exception {
+        new App();
+    }
+    
+    private void Nortification(int stat){
+        icon.displayMessage("エラー", "rdpファイルを作成できませんでした("+stat+")", MessageType.WARNING);
+    }
+    private void Nortification(String ip){
+        icon.displayMessage("プライベートIPアドレス の取得", ip+"\nrdpファイルを作成しました", MessageType.INFO);
+    }
+        
+    private String getIP(Enumeration<NetworkInterface> ifs) {
         String result = null;
 
         if (null != ifs){
